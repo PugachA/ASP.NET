a25fff8260b286900334f3f35a0977627a27a458
Pugach Alexander <fotone97@yandex.ru>
2026-01-20 21:34:26 +0300

    net 10

---
+++ Homeworks/02 Знакомство с проектом и реализация CRUD-операций/src/PromoCodeFactory.Core/Abstractions/Repositories/IRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PromoCodeFactory.Core.Domain;
+
+namespace PromoCodeFactory.Core.Abstractions.Repositories;
+
+public interface IRepository<T> where T: BaseEntity
+{
+    Task<IEnumerable<T>> GetAllAsync();
+
+    Task<T> GetByIdAsync(Guid id);
+}
---
+++ Homeworks/02 Знакомство с проектом и реализация CRUD-операций/src/PromoCodeFactory.Core/Domain/BaseEntity.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace PromoCodeFactory.Core.Domain;
+
+public abstract  class BaseEntity
+{
+    public Guid Id { get; set; }
+}
